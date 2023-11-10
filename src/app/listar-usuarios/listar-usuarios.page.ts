cf2d916236642c72d892703cac3d00f879c56fc0
varoglx <114885048+varoglx@users.noreply.github.com>
2023-11-10 15:58:07 -0300

    o
    o

--- src/app/listar-usuarios/listar-usuarios.page.ts
+++ src/app/listar-usuarios/listar-usuarios.page.ts
@@ -10,7 +10,7 @@ export class ListarUsuariosPage implements OnInit {
   jsonData: any[];
   constructor(private http: HttpClient) { }
   ngOnInit() {
-    const url = 'http://localhost:5000/get_data_usuarios'; // Reemplaza esto con la URL real de tu JSON
+    const url = 'http://18.230.155.252:80/get_data_usuarios'; // Reemplaza esto con la URL real de tu JSON
   
     this.http.get(url).subscribe((data: any) => {
       console.log('Datos JSON:', data);
@@ -30,12 +30,12 @@ export class ListarUsuariosPage implements OnInit {
     .then(response => response.json())
     .then(data => {
       console.log('Respuesta del servidor:', data);
-      this.ngOnInit();
+      
     });
   }
 
   deleteUser(id: number) {
-    const deleteUrl = 'http://localhost:5000/delete_usuario/' + id; // Reemplaza con la URL correcta
+    const deleteUrl = 'http://18.230.155.252:80/delete_usuario/' + id; // Reemplaza con la URL correcta
     console.log('enviado')
     fetch(deleteUrl, {
       method: 'POST',
@@ -43,7 +43,7 @@ export class ListarUsuariosPage implements OnInit {
     .then(response => response.json())
     .then(data => {
       console.log('Respuesta del servidor:', data);
-      this.ngOnInit();
+      
     });
   }
   
